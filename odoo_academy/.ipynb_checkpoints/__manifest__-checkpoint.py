d8154830d1f608f0c8e642c657f1b023e67c0bd5
JRichmond <jrichmond@prointeccr.com>
2023-01-24 03:10:26 +0000

    [ADD] Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Acadent app to manage Training""",
+    
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'www.prointeccr.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends':  ['base'],
+    
+    'data': [
+    
+    
+    ],
+    
+    'demo': [
+    
+        
+    ],
+    
+}
